a781ce5112238aad62c5379acca16ca6d30ec64b
alecyalcin <alecyalcin@gmail.com>
2024-12-23 19:19:24 -0300

    Conexão com o banco de dados estabelecida

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,19 @@
 import { createServer } from "http";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  console.log(err);
+});
 
 const port = 3000;
 
