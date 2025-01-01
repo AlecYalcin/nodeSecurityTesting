64aaf906d4391655d0eff0e36b4bdfaa2d353501
alecyalcin <alecyalcin@gmail.com>
2025-01-01 15:56:05 -0300

    Adicionado controller de pagamentos

---
+++ backend/src/controllers/payment_controller.ts
@@ -0,0 +1,120 @@
+// Packages
+import { QueryTypes } from "sequelize";
+
+// Database Config
+import { sequelize } from "../database/config/database";
+
+// Model
+import Payment from "../database/models/payments";
+
+// Interface
+import { BookAttribute } from "../database/models/books";
+import { UserAttribute } from "../database/models/users";
+
+class PaymentController {
+  createPayment = async (req: any, res: any) => {
+    const { user_id, book_id, quantity } = req.body;
+
+    // Verificar se o USUÁRIO existe
+    const user = await sequelize.query<UserAttribute>(
+      `SELECT * FROM users WHERE id=${user_id}`,
+      {
+        type: QueryTypes.SELECT,
+        plain: true,
+      }
+    );
+
+    if (!user) {
+      res.status(404).send({ message: "Usuário não encontrado." });
+      return;
+    }
+
+    // Verificar se o LIVRO existe
+    const book = await sequelize.query<BookAttribute>(
+      `SELECT * FROM books WHERE id=${book_id}`,
+      {
+        type: QueryTypes.SELECT,
+        plain: true,
+      }
+    );
+
+    if (!book) {
+      res.status(404).send({ message: "Livro não encontrado." });
+      return;
+    }
+
+    // Verificar se a QUANTIDADE é POSSÍVEL
+    if (book.stock < quantity) {
+      res.status(400).send({
+        message: "Não há quantidade de livros disponível.",
+      });
+      return;
+    }
+
+    try {
+      // Realizando o Pagamento
+      const payment = await Payment.create({
+        user_id: user_id,
+        book_id: book_id,
+        total_price: book.price * quantity,
+        quantity: quantity,
+      });
+
+      // Retirando do Estoque
+      if (payment) {
+        await sequelize.query(
+          `UPDATE books SET stock = ${
+            book.stock - quantity
+          } WHERE id = ${book_id}`
+        );
+      }
+
+      return res.status(201).json({ message: "Sucesso ao criar pagamento!" });
+    } catch (error) {
+      return res.status(400).json({ message: "Falha ao criar pagamento. " });
+    }
+  };
+
+  readPayment = async (req: any, res: any) => {
+    const { id, total, date } = req.query;
+
+    // Query de Busca
+    let query = "SELECT * FROM payments";
+
+    // Alterando QUERY com Where
+    if (id || total || date) {
+      query = query + " WHERE ";
+      if (id) query = query + `id=${id} `;
+      if (total) query = query + `total_price=${total} `;
+      if (date) query = query + `date=${date} `;
+    }
+
+    try {
+      const [payments, metadata] = await sequelize.query(query);
+      return res.status(200).json(payments);
+    } catch (error) {
+      return res.status(400).json({ message: "Falha ao pesquisar pagamento." });
+    }
+  };
+
+  deletePayment = async (req: any, res: any) => {
+    const id = req.params.id;
+
+    try {
+      const payment = await Payment.destroy({
+        where: {
+          id: id,
+        },
+      });
+
+      return res.status(200).json({ message: "Sucesso ao excluir pagamento!" });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ message: "Falha ao excluir pagamento." });
+    }
+  };
+}
+
+const controller = new PaymentController();
+
+export default controller;
--- backend/src/routes/payment.ts
+++ backend/src/routes/payment.ts
@@ -1,131 +1,21 @@
-// Packages
-import { QueryTypes } from "sequelize";
 import express from "express";
-
-// Modules
-import { sequelize } from "../database/config/database";
-
-// Interface
-import { BookAttribute } from "../database/models/books";
-import { UserAttribute } from "../database/models/users";
-
-// Models
-import Payment from "../database/models/payments";
+import PaymentController from "../controllers/payment_controller";
 
 const router = express.Router();
 
 // Criação de Pagamento
-router.post("/create", async (req, res) => {
-  const user_id = req.body.user_id;
-  const book_id = req.body.book_id;
-  const quantity = req.body.quantity;
-
-  // Verificar se o USUÁRIO existe
-  const user = await sequelize.query<UserAttribute>(
-    `SELECT * FROM users WHERE id=${user_id}`,
-    {
-      type: QueryTypes.SELECT,
-      plain: true,
-    }
-  );
-
-  if (!user) {
-    res.status(404).send({ message: "Usuário não encontrado." });
-    return;
-  }
-
-  // Verificar se o LIVRO existe
-  const book = await sequelize.query<BookAttribute>(
-    `SELECT * FROM books WHERE id=${book_id}`,
-    {
-      type: QueryTypes.SELECT,
-      plain: true,
-    }
-  );
-
-  if (!book) {
-    res.status(404).send({ message: "Livro não encontrado." });
-    return;
-  }
-
-  // Verificar se a QUANTIDADE é POSSÍVEL
-  if (book.stock < quantity) {
-    res.status(400).send({
-      message: "Não há quantidade de livros disponível.",
-    });
-    return;
-  }
-
-  const total_price = book.price * quantity;
-  try {
-    // Realizando o Pagamento
-    const payment = await Payment.create({
-      user_id: user_id,
-      book_id: book_id,
-      total_price: total_price,
-      quantity: quantity,
-    });
-
-    // Retirando do Estoque
-    if (payment) {
-      await sequelize.query(
-        `UPDATE books SET stock = ${
-          book.stock - quantity
-        } WHERE id = ${book_id}`
-      );
-    }
-
-    res.sendStatus(201).send();
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400).send({ message: "Aconteceu um erro. " });
-  }
-});
+router.post("/create", async (req, res) =>
+  PaymentController.createPayment(req, res)
+);
 
 // Listagem de Pagamentos
-router.get("/list", async (req, res) => {
-  const id = req.query.id;
-  const total = req.query.total;
-  const date = req.query.date;
-
-  // Query de Busca
-  let query = "SELECT * FROM payments";
-
-  // Alterando QUERY com Where
-  if (id || total || date) {
-    query = query + " WHERE ";
-    if (id) query = query + `id=${id} `;
-    if (total) query = query + `total_price=${total} `;
-    if (date) query = query + `date=${date} `;
-  }
-
-  try {
-    const [payments, metadata] = await sequelize.query(query);
-    res.status(200).send(payments);
-  } catch (error) {
-    console.log(error);
-    res.status(400).send({ message: "Aconteceu um erro na pesquisa." });
-  }
-});
+router.get("/list", async (req, res) =>
+  PaymentController.readPayment(req, res)
+);
 
 // Destuir pagamentos
-router.delete("/:id", async (req, res) => {
-  const id = req.params.id;
-
-  try {
-    const payment = await Payment.destroy({
-      where: {
-        id: id,
-      },
-    });
-
-    res.status(200).send({ message: "Pagamento excluído com sucesso!" });
-  } catch (error) {
-    console.log(error);
-    res
-      .status(400)
-      .send({ message: "Aconteceu um erro ao excluir o pagamento." });
-  }
-});
+router.delete("/:id", async (req, res) =>
+  PaymentController.deletePayment(req, res)
+);
 
 export default router;
