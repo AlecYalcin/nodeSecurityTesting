d23528f29e7e695915ad6182e0b89be46135be84
alecyalcin <alecyalcin@gmail.com>
2025-01-01 15:34:11 -0300

    Criado controller da rota book

---
+++ backend/src/controllers/book_controller.ts
@@ -0,0 +1,80 @@
+// Database Config
+import { sequelize } from "../database/config/database";
+// Model
+import Book from "../database/models/books";
+
+class BookController {
+  // Create Book
+  createBook = async (req: any, res: any) => {
+    try {
+      await Book.create(req.body);
+      return res.status(201).json({ message: "Sucesso ao criar o livro." });
+    } catch (error) {
+      return res.status(400).json({ message: "Falha ao criar o livro." });
+    }
+  };
+
+  // Read/List/Search Book
+  readBook = async (req: any, res: any) => {
+    // Parâmetros de Busca
+    const { id, title, author, price, stock } = req.query;
+
+    // Query de Busca
+    let query = "SELECT * FROM books";
+
+    // Alterando QUERY com Where
+    if (id || title || author || price || stock) {
+      query = query + " WHERE ";
+      if (id) query = query + `id=${id} `;
+      if (title) query = query + `title=${title} `;
+      if (author) query = query + `author=${author} `;
+      if (price) query = query + `price<=${price} `;
+      if (stock) query = query + `stock<=${stock} `;
+    }
+
+    try {
+      const [books, metadata] = await sequelize.query(query);
+      return res.status(200).send(books);
+    } catch (error) {
+      return res.status(404).json({ message: "Falha ao buscar o livro." });
+    }
+  };
+
+  // Update Book
+  updateBook = async (req: any, res: any) => {
+    const id = req.params.id;
+
+    try {
+      const book = await Book.update(req.body, {
+        where: {
+          id: id,
+        },
+      });
+
+      return res.status(200).send({ message: "Sucesso ao atualizar o livro" });
+    } catch (error) {
+      return res.status(400).send({ message: "Falha ao atualizar o livro." });
+    }
+  };
+
+  // Delete Book
+  deleteBook = async (req: any, res: any) => {
+    const id = req.params.id;
+
+    try {
+      const book = await Book.destroy({
+        where: {
+          id: id,
+        },
+      });
+
+      res.status(200).send({ message: "Livro excluído com sucesso! " });
+    } catch (error) {
+      res.status(400).send({ message: "Aconteceu um erro ao excluir Livro. " });
+    }
+  };
+}
+
+const controller = new BookController();
+
+export default controller;
--- backend/src/routes/books.ts
+++ backend/src/routes/books.ts
@@ -1,73 +1,20 @@
 import express from "express";
 import { sequelize } from "../database/config/database";
 import Book from "../database/models/books";
+import BookController from "../controllers/book_controller";
 
 const router = express.Router();
 
 // CREATE
-router.post("/", async (req, res) => {
-  const book = await Book.create(req.body);
-  res.status(201).send({ message: "O livro foi criado!" });
-});
+router.post("/", async (req, res) => BookController.createBook(req, res));
 
 // READ
-router.get("/", async (req, res) => {
-  const id = req.query.id;
-  const title = req.query.title;
-  const author = req.query.author;
-
-  // Query de Busca
-  let query = "SELECT * FROM books";
-
-  // Alterando QUERY com Where
-  if (id || title || author) {
-    query = query + " WHERE ";
-    if (id) query = query + `id=${id} `;
-    if (title) query = query + `title=${title} `;
-    if (author) query = query + `author=${author} `;
-    console.log(query);
-  }
-
-  try {
-    const [books, metadata] = await sequelize.query(query);
-    res.status(200).send(books);
-  } catch (error) {
-    res.status(400).send({ message: "Aconteceu um erro na pesquisa." });
-  }
-});
+router.get("/", async (req, res) => BookController.readBook(req, res));
 
 // UPDATE
-router.patch("/:id", async (req, res) => {
-  const id = req.params.id;
-
-  try {
-    const book = await Book.update(req.body, {
-      where: {
-        id: id,
-      },
-    });
-
-    res.status(200).send({ message: "Livro atualizado com sucesso!" });
-  } catch (error) {
-    res.status(400).send({ message: "Aconteceu um erro ao atualizar Livro." });
-  }
-});
+router.patch("/:id", async (req, res) => BookController.updateBook(req, res));
 
 // DELETE
-router.delete("/:id", async (req, res) => {
-  const id = req.params.id;
-
-  try {
-    const book = await Book.destroy({
-      where: {
-        id: id,
-      },
-    });
-
-    res.status(200).send({ message: "Livro excluído com sucesso! " });
-  } catch (error) {
-    res.status(400).send({ message: "Aconteceu um erro ao excluir Livro. " });
-  }
-});
+router.delete("/:id", async (req, res) => BookController.deleteBook(req, res));
 
 export default router;
